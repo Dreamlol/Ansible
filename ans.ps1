c2b01b2a5002ed0eb65e4b916a15a749f2370b0f
Dreamlol <Dreamorigin01@gmail.com>
2017-09-01 06:43:34 -0700

    Update ans.ps1

--- ans.ps1
+++ ans.ps1
@@ -6,60 +6,71 @@
 #    & ansible-cygwin-installer.ps1
 #
 # Run from cmd with
-#    powershell -ExecutionPolicy bypass "ansible-cygwin-installer.ps1"
+#    powershell -ExecutionPolicy bypass ansible-cygwin-installer.ps1
 #
-
 #
 # User variables. These may be changed to suit your environment.
 #
-
 $storageDir = $pwd
-$cygwinHome = "c:\cygwin"
-$cygwinUrlRoot = "http://cygwin.com"
-$getPipUrlRoot = "https://bootstrap.pypa.io"
-$cygwinMirror = "http://cygwin.mirrors.pair.com"
+$cygwinHome = "ccygwin"
+$cygwinUrlRoot = "http://cygwin.com/"
+$getPipUrlRoot = "httpsbootstrap.pypa.io"
+$cygwinMirror = "http://cygwin.mirrors.constant.com"
 
-#
-# You shouldn't normally need to change anything below here
-#
 
-if ($ENV:PROCESSOR_ARCHITECTURE -eq 'AMD64') {
+
+# Make temp directory
+$path = "$pwd/temp_cy/"
+if(!(test-path $path)){
+New-Item -ItemType Directory -Path $path
+}
+
+echo $env:PROCESSOR_ARCHITECTURE
+if ($env:PROCESSOR_ARCHITECTURE -eq 'AMD64') {
     $cygwinSetupExe = "setup-x86_64.exe"
-    $url = "$cygwinUrlRoot/$cygwinSetupExe"
-    $file = "$storageDir\$cygwinSetupExe"
-} elseif ($ENV:PROCESSOR_ARCHITECTURE -eq 'x86') {
+    $url = "$cygwinUrlRoot$cygwinSetupExe"
+	echo $url
+	$file = "$path\$cygwinSetupExe"
+	echo $file
+	}
+	
+	
+	
+ elseif ($env:PROCESSOR_ARCHITECTURE -eq 'x86') {
     $cygwinSetupExe = "setup-x86.exe"
-    $url = "$cygwinUrlRoot/$cygwinSetupExe"
-    $file = "$storageDir\$cygwinSetupExe"
+    $url = "$cygwinUrlRoot$cygwinSetupExe"
+    $file ="$storageDir$cygwinSetupExe"
 } else {
     echo 'Unknown processor architecture'
     exit
 }
 
 # Fully qualified path to Cygwin setup.exe
-$cygwinSetupPath = "$storageDir\$cygwinSetupExe"
+$cygwinSetupPath = "$path$cygwinSetupExe"
 
 # Download Cygwin setup.exe, if it doesn't already exist
 if ( ! ( Test-Path -Path $cygwinSetupPath -PathType Leaf ) ) {
-    $webclient = New-Object System.Net.WebClient
-    $webclient.DownloadFile($url,$file)
-}
+	$webclient = New-Object System.Net.WebClient
+    $webclient.DownloadFile($url, $file)
 
-$cygwinSetupArgs = '--no-admin', '-q', '-R', "$cygwinHome", '-s', "$cygwinMirror", '--packages="wget,python,git,vim,openssh,openssl,openssh-devel,libsasl2,ca-certificates,python-crypto,python-openssl,python-setuptools,dash,rebase"'
+}
+#http://cygwin.com/setup-x86_64.exe
+#C:\setup-x86_64.exe
+$cygwinSetupArgs = '--no-admin', '-q', '-R', $cygwinHome, '-s', $cygwinMirror, '--packages=wget, curl'
 Start-Process -FilePath $cygwinSetupPath -ArgumentList $cygwinSetupArgs -Wait
 
 # Add cygwin bin dir to path
-$ENV:PATH="$cygwinHome\bin;$ENV:PATH"
+#$ENVPATH=$cygwinHomebin;$ENVPATH
 
 # Install pip
-Start-Process -FilePath $cygwinHome\bin\bash.exe -ArgumentList '-c', """wget.exe $getPipUrlRoot/get-pip.py""" -Wait -NoNewWindow
-Start-Process -FilePath $cygwinHome\bin\bash.exe -ArgumentList '-c', '"python2.7.exe get-pip.py"' -Wait -NoNewWindow
+#Start-Process -FilePath $cygwinHomebinbash.exe -ArgumentList '-c', wget.exe $getPipUrlRootget-pip.py -Wait -NoNewWindow
+#Start-Process -FilePath $cygwinHomebinbash.exe -ArgumentList '-c', 'python2.7.exe get-pip.py' -Wait -NoNewWindow
 
 # Fix fork() errors on some systems
-Start-Process -FilePath $cygwinHome\bin\dash.exe -ArgumentList '-c', '"/usr/bin/rebaseall -v"' -Wait -NoNewWindow
+#Start-Process -FilePath $cygwinHomebindash.exe -ArgumentList '-c', 'usrbinrebaseall -v' -Wait -NoNewWindow
 
 # Install Ansible via pip
-Start-Process -FilePath $cygwinHome\bin\bash.exe -ArgumentList '-c', '"pip2.7 install ansible"' -Wait -NoNewWindow
+#Start-Process -FilePath $cygwinHomebinbash.exe -ArgumentList '-c', 'pip2.7 install ansible' -Wait -NoNewWindow
 
 # Run Ansible from outside of Cygwin shell
-Start-Process -FilePath $cygwinHome\bin\bash.exe -ArgumentList '-c', '"ansible --version"' -Wait -NoNewWindow
+#Start-Process -FilePath $cygwinHomebinbash.exe -ArgumentList '-c', 'ansible --version' -Wait -NoNewWindow
