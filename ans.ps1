a5aee48a90dcc82336c4a70996485aaf20928555
Dreamlol <Dreamorigin01@gmail.com>
2017-09-01 16:45:52 +0300

    Update ans.ps1

--- ans.ps1
+++ ans.ps1
@@ -3,10 +3,10 @@
 #
 # Run from Powershell
 #    Set-ExecutionPolicy bypass
-#    & ansible-cygwin-installer.ps1
+#    & ans.ps1
 #
 # Run from cmd with
-#    powershell -ExecutionPolicy bypass ansible-cygwin-installer.ps1
+#    powershell -ExecutionPolicy bypass ans.ps1
 #
 #
 # User variables. These may be changed to suit your environment.
